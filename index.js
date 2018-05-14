785db628d6c2118aca1278c46f1ad11d86238f0e
Sarah Erin Roach <saraherinroach@gmail.com>
2018-05-14 21:18:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function logWhisper(string) {
   console.log(`${string}`.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.to)
+  if (string.toLowerCase() === string) {return "I can't hear you!"}
+  else if (string.toUpperCase() === string) {return "YES INDEED!"}
+  else if (string === "I love you, Grandma") {return "I love you, too."}
 }
